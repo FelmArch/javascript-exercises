481e79ba7d4f958243037bd2df7cda28813a0eb6
FelmArch <ferrlinda.dl@gmail.com>
2026-04-11 09:59:11 +0100

    fix:Finaliza o exercicio 05 joinStrings

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -1,9 +1,17 @@
+
 const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
+// adicionando novas variaves
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+// const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+
+// otimizando
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
 
 console.log(greeting);
 
@@ -12,5 +20,8 @@ module.exports = {
   birthYear,
   thisYear,
   firstName,
-  lastName
+  lastName,
+  // as duas novas variaveis
+  fullName,
+  age
 }
