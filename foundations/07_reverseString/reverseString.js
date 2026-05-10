bf84bab45ecd7b4414cb1ae8603089949ebd64f1
FelmArch <ferrlinda.dl@gmail.com>
2026-05-10 10:32:06 +0200

    feat: resolva reverseString 07 exercicio

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(texto) {
+    return texto.split('').reverse().join('');
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
