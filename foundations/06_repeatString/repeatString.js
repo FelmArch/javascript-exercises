30c2058ca3f229f9b74cb9a057e3d4d434cd4840
FelmArch <ferrlinda.dl@gmail.com>
2026-05-10 10:09:39 +0200

    feat: resolva o exercicio 06 repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(texto, num) {
+    // 1. Verificar se oo numero e negativo
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let resultado = '';
 
+    for (let i = 0; i < num; i++) {
+        resultado += texto;
+    }
+
+    return resultado;
 };
 
 // Do not edit below this line
