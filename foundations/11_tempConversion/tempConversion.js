3cf2347189f7dea4adf0c236e8fa9b3e01a38e02
FelmArch <ferrlinda.dl@gmail.com>
2026-05-10 13:55:44 +0200

    feat: resolva exercicio 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) * (5 / 9);
+  // Arredondar para 1 casa decimal
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * (9 / 5) + 32);
+  // Arredondar para 1 casa decimal
+  return Math.round(fahrenheit * 10) / 10;
+};
+
+// No final do ficheiro, o export será assim (como o README mencionou):
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
 };
 
 // Do not edit below this line
