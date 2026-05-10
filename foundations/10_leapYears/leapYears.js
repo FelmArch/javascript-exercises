cfd6fbedec18b0643896d962780b2567aff45b59
FelmArch <ferrlinda.dl@gmail.com>
2026-05-10 13:50:43 +0200

    feat: resolve exercicio 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(ano) {
+  // Traduzindo a lógica:
+  // É bissexto se (divisível por 4 E NÃO for divisível por 100)
+  // OU se for divisível por 400.
+  
+  const divisivelPorQuatro = ano % 4 === 0;
+  const divisivelPorCem = ano % 100 === 0;
+  const divisivelPorQuatrocentos = ano % 400 === 0;
 
+  if (divisivelPorQuatro && (!divisivelPorCem || divisivelPorQuatrocentos)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
